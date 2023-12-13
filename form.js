49ac41ff73f4efb5238143db81e11ec59bf83098
DTTBfine <dothanhbinhdayah@gmail.com>
2023-12-13 09:55:13 +0700

    new commit

--- form.js
+++ form.js
@@ -4,37 +4,55 @@ var username = document.querySelector('#Username');
 var password = document.querySelector('#Password'); 
 var confirmPassword = document.querySelector('#confirmPassword');
 var role = document.querySelector('#Role');
+var form = document.querySelector('form');
+
 
 function showError(input, message) {
-    let parent = input.parentElement
-    let small = parent.querySelector('small')
-    parent.classList.add('error')
-    small.innerText = message
+    let parent = input.parentElement;
+    let small = parent.querySelector('small');
+    parent.classList.add('error');
+    small.innerText = message;
 }
 
 function showSuccess(input) {
-    let parent = input.parentElement
-    let small = parent.querySelector('small')
-    parent.classList.remove('error')
-    small.innerText = ''
+    let parent = input.parentElement;
+    let small = parent.querySelector('small');
+    parent.classList.remove('error');
+    small.innerText = '';
 }
 
 function checkEmptyError(listInput) {
+    let isEmptyError = false
     listInput.forEach(input => {
-        input.value = input.value.trim()
+        input.value = input.value.trim();
 
-        if(!input.value){
-            showError(input,'Không được để trống')
+        if (!input.value) {
+            isEmptyError = true
+            showError(input, 'Không được để trống');
+        } else {
+            showSuccess(input);
         }
-        else {
-            showSuccess(input)
-        }
-    })
+    });
 
+    return isEmptyError;
 }
 
-form.addEventListener('submit',function(e){
-    e.preventDefault()
+function checkEmailError(input) {
+    const regexEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    input.value = input.value.trim();
 
-    checkEmptyError([phonenumber, email, username, password, confirmPassword ])
-})
+    let isEmailError = !regexEmail.test(input.value);
+
+    if(!isEmailError) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Sai định dạng Email');
+    }
+    return isEmailError;
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let isEmptyError = checkEmptyError([phonenumber, email, username, password, confirmPassword]);
+});
--- main.js
+++ main.js
@@ -3,28 +3,33 @@ var introduce = document.getElementById("Gioi-thieu");
 var instruction = document.getElementById("Huong-dan");
 var contact = document.getElementById("Lien-he");
 
-document.getElementById("Homepage_link").addEventListener("click", function() {
+var Homepage_link = document.getElementById("Homepage_link")
+var Introduce_link = document.getElementById("Introduce_link")
+var Instruction_link = document.getElementById("Instruction_link")
+var Contact_link = document.getElementById("Contact_link")
+
+Homepage_link.addEventListener("click", function() {
     homepage.style.display = 'flex';
     introduce.style.display = 'none';
     instruction.style.display = 'none';
     contact.style.display = 'none';
 });
 
-document.getElementById("Introduce_link").addEventListener("click", function() {
+Introduce_link.addEventListener("click", function() {
     homepage.style.display = 'none';
     introduce.style.display = 'flex';
     instruction.style.display = 'none';
     contact.style.display = 'none';
 });
 
-document.getElementById("Instruction_link").addEventListener("click", function() {
+Instruction_link.addEventListener("click", function() {
     homepage.style.display = 'none';
     introduce.style.display = 'none';
     instruction.style.display = 'flex';
     contact.style.display = 'none';
 });
 
-document.getElementById("Contact_link").addEventListener("click", function() {
+Contact_link.addEventListener("click", function() {
     homepage.style.display = 'none';
     introduce.style.display = 'none';
     instruction.style.display = 'none';
